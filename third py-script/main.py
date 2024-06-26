ba88cf22a7e7b37b81e3d32558bb065783338710
Alok <karnaalok48@gmail.com>
2024-06-26 22:47:01 +0545

    python update

--- third py-script/main.py
+++ third py-script/main.py
@@ -1,2 +1,13 @@
-def hello():
-    print("Hello there")
+from pyscript import document
+
+def score_update():
+    player1_score = 0
+    player2_score = 0
+    # run = document.querySelector('#')
+    run.addEventListener('click', lambda: player1_score += 1)
+
+
+
+    
+    # output_div = document.querySelector('#counter-result')
+    # output_div.textContent = f"Word count: {word_count}"
