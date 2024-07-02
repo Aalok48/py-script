14cd25bc844ad781d7c4e291955e8894b1cba024
Alok <karnaalok48@gmail.com>
2024-07-02 20:37:11 +0545

    main.py

--- third py-script/main.py
+++ third py-script/main.py
@@ -1,13 +1,15 @@
+import js
 from pyscript import document
 
-def score_update():
-    player1_score = 0
-    player2_score = 0
-    # run = document.querySelector('#')
-    run.addEventListener('click', lambda: player1_score += 1)
+def score_update(event):
+    player1_score = js.document.getElementById('player1_score')
+    player2_score = js.document.getElementById('player2_score')
+
+    output_div1 = document.querySelector('#player1_score_result')
+    output_div1.textContent = player1_score
+
+    output_div2 = document.querySelector('#player2_score_result')
+    output_div2.textContent = player2_score
 
 
-
-    
-    # output_div = document.querySelector('#counter-result')
-    # output_div.textContent = f"Word count: {word_count}"
+# document.querySelector('#count').addEventListener('click', score_update)
