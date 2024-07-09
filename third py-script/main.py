a0c7f984c9133ec21157782e78d9fe2e8ef08cdf
Alok <karnaalok48@gmail.com>
2024-07-09 21:41:35 +0545

    funct

--- third py-script/main.py
+++ third py-script/main.py
@@ -1,18 +1,10 @@
-import js
 from pyscript import document
 
 def score_update(event):
-    # Get the selected radio button for player 1
-    player1_selected = js.document.querySelector('#player1_score input[name="run"]:checked')
-    player1_score = int(player1_selected.value)  # Convert to integer
+    result = 0
+    player_score_ = document.queryselector('#player_score')
 
-    # Get the selected radio button for player 2
-    player2_selected = js.document.querySelector('#player2_score input[name="run"]:checked')
-    player2_score = int(player2_selected.value)  # Convert to integer
 
-    # Update the score display
-    output_div1 = document.querySelector('#player_result')
-    output_div1.textContent = f"Alok karn: {player1_score}"
-
-    output_div2 = document.querySelector('#player_result')
-    output_div2.textContent = f"Subhash Mandal: {player2_score}"
+    
+    output_div = document.querySelector('#player_result')
+    output_div.innerText = result
